57d81f5321bd32ba761130253075956d710bc1d7
Peritia <Peritia@peritia.space>
2025-08-05 10:48:47 +0200

    test

--- flake.nix
+++ flake.nix
@@ -1,32 +1,17 @@
 {
-  description = "Flake for NixOS-95 theme";
+  description = "My NixOS config";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    systems.url = "github:nix-systems/default";
-
-    # Home Manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
+    nixos95 = {
+      url = "github:Peritia-System/NixOS-95-BETA";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
   };
 
-
-  outputs = inputs @ {
-    flake-parts,
-    systems, 
-    nixpkgs,
-    home-manager,
-    ...
-  }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ./nixos95 ];
-      systems = import systems;
+  outputs = { self, nixpkgs, ... }: {
+    modules = {
+      taskbar = ./nixos95/taskbar.nix;
     };
+  };
 }
