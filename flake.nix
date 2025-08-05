9d25027039dd520e600c190bd37fdb41fd368d29
Peritia <Peritia@peritia.space>
2025-08-05 10:18:40 +0200

    test

--- flake.nix
+++ flake.nix
@@ -1,46 +1,27 @@
 {
-  description = "NixOS-95";
+  description = "Flake for NixOS-95 theme";
 
-  ################################################################
-  # 🔗 Inputs
-  ################################################################
   inputs = {
-    # Core NixOS packages
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    systems.url = "github:nix-systems/default";
 
     # Home Manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-  };
-
-  ################################################################
-  # 🚀 Outputs
-  ################################################################
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  }: {
-
-    nixosModules.nixos95 = import ./nixos95;
-
-    nixosConfigurations = {
-      ############################################################
-      # 💻 Default Host
-      ############################################################
-      default = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs self;
-          host = "default";
-        };
-        modules = [
-          ./Configurations/Hosts/Default/configuration.nix
-        ];
-      };
-      
+    
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+
   };
+
+  outputs =
+    { flake-parts, systems, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ./nixos95 ];
+      nixosModules.nixos95 = import ./nixos95;
+      systems = import systems;
+    };
 }
