91ef902d958f360871fb24b4c4e93f756e4a9403
Peritia <Peritia@peritia.space>
2025-08-05 10:56:53 +0200

    aaaaaaaaa

--- flake.nix
+++ flake.nix
@@ -3,15 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos95 = {
-      url = "github:Peritia-System/NixOS-95-BETA";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, ... }: {
     modules = {
-      taskbar = ./nixos95/taskbar.nix;
+      taskbar = ./nixos95;
     };
   };
 }
